6a6a09145a7948f0ff1bbc99507941723648cff1
Mason Fischer <mason@thoughtbot.com>
2017-01-29 11:47:42 -0600

    Send JSON

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var request = require('request');
 var Web3 = require('web3');
 var BigNumber = require('bignumber.js');
 var web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETHEREUM_RPC_URL));
+var _ = require('lodash');
 var express = require('express');
+var cors = require('cors');
 var app = express();
+
+
+app.use(cors())
 
 const SOURCE_KEY = process.env.SOURCE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
@@ -54,12 +59,12 @@ function pay({transactionHash, accountId, amount}){
 
 app.get('/federation', function (req, res) {
   const [ address, domain ] = req.query.q.split('*')
-  res.send(JSON.stringify({
+  res.json({
     stellar_address:  req.query.q,
-    account_id: process.env.ASSET_ISSUER,
-    memo_type: "text",
-    memo: address
-  }));
+    account_id: "GD7IW7QG5EHE34FZSUVYQP3LO577J72TXYRCBCZEPRWBKJOPZOCTER2Q",
+    memo_type: "hash",
+    memo: address.substring('2') + _.repeat("0", 24),
+  });
 })
 
 app.listen(process.env.PORT, function () {
